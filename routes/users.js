ff4c5b6f0fc8c5d37cd8cd6024b17d253c3e3518
Steven Sarria <ssarr012@fiu.edu>
2021-01-21 15:54:17 -0500

    added decryption

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@
 const router = require('express').Router();
 let User = require('../models/user.models');
 //assigns created user password a unique token as a securty feature
-let jwt = require('jsonwebtoken')
+let jwt = require('jsonwebtoken');
+//enable bcrypt to decrypt user passwords for security
+const bcrypt = require('bcrypt');
 
 //now we create our first route
 router.route('/').get((req, res) => {
@@ -16,36 +18,40 @@ router.route('/').get((req, res) => {
 //second endpoint handles incoming http post requests.
 //this endpoint handles users creating an account.
 router.route('/signup').post((req, res) => {
-    //make sure there is no other email in the database that matches the one being created
-    User.findOne({ email: req.body.email })
-        .then((user) => {
-            //if email is already found, user gets an error
-            if (user) return res.status(400).send("Email already exists");
-            else {
-                //user is given a text field where they need to input all requirements to create a user
-                return User.create({
-                    email: req.body.email,
-                    firstname: req.body.firstname,
-                    lastname: req.body.lastname,
-                    password: req.body.password,
-                    //once user is succesfully created they're assinged a special token
-                }).then((user) => {
-                    let token = jwt.sign({ id: user._id }, 'shhhh')
-                    //once token is given, I would see the user email, token and usertype on my terminal and database
-                    res.status(200).json({
-                        token,
-                        user: {
-                            email: user.email,
-                            userType: user.userType
-                        }
+    //inputting bcrypt to hash passwords for security. The 10 is how many digits will hash.
+    bcrypt.hash(req.body.password, 10, function (err, hash) {
+        //make sure there is no other email in the database that matches the one being created
+        User.findOne({ email: req.body.email })
+            .then((user) => {
+                //if email is already found, user gets an error
+                if (user) return res.status(400).send("Email already exists");
+                else {
+                    //user is given a text field where they need to input all requirements to create a user
+                    return User.create({
+                        email: req.body.email,
+                        firstname: req.body.firstname,
+                        lastname: req.body.lastname,
+                        //stores hash in our password DB
+                        password: hash,
+                        //once user is succesfully created they're assinged a special token
+                    }).then((user) => {
+                        let token = jwt.sign({ id: user._id }, 'shhhh')
+                        //once token is given, I would see the user email, token and usertype on my terminal and database
+                        res.status(200).json({
+                            token,
+                            user: {
+                                email: user.email,
+                                userType: user.userType
+                            }
+                        })
                     })
-                })
-            }
-        })
-        //This occurs when the user has an error on their end.
-        .catch((err) => {
-            res.status(500).send('Whoops!')
-        })
+                }
+            })
+            //This occurs when the user has an error on their end.
+            .catch((err) => {
+                res.status(500).send('Whoops!')
+            })
+    });
 })
 
 //login CRUD function
@@ -53,24 +59,20 @@ router.route("/login").post((req, res) => {
     User.findOne({ email: req.body.email })
         .then((user) => {
             if (!user) {
-                alert("Invalid credentials!");
-                res.status(403).send("Invalid credentials");
+                res.status(403).send("Invalid credentials 2");
             }
-            else if (user.password === req.body.password) {
-                var token = jwt.sign({ id: user._id }, 'shhh')
-                res.status(200).json({
-                    token,
-                    user: {
-                        email: user.email,
-                        userType: user.userType
-                    }
+            else if (user) {
+                bcrypt.compare(req.body.password, user.password, function (err, match) {
+                    if (err) res.status(500).send("Whoops!")
+                    if (match) {
+                        var token = jwt.sign({ id: user._id }, 'shhh')
+                        res.json({ token: token })
+                    } else res.status(403).send("Invalid credentials 3")
                 })
-                console.log(token)
+            } else {
+                res.status(403).send("Invalid credentials 4")
             }
-            else {
-                res.status(403).send("Invalid credentials")
-            }
-        })
-})
+        });
+});
 
 module.exports = router;
