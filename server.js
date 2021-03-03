5dccc1fa23d720f9e8c90e820250f9e7e74d8aae
Steven Sarria <stevensarria@Matthews-MBP.attlocal.net>
2021-03-03 15:09:32 -0500

    added app.get

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedT
 const usersRouter = require('./routes/users.js');
 const reviewsRouter = require('./routes/reviews.js')
 
+//heroku post
+app.get("/" , (req, res) => {
+    res.send("Hello to Backlot API")
+})
+
 //endpoints that have to be hit to see the database information (localhost5000/users show all users, etc)
 app.use('/reviews', reviewsRouter);
 app.use('/users', usersRouter)
